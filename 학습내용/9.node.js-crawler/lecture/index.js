b17e6df1e8614cb079b3dd468c17e7adc0c468b9
Ungzzi <qhshqhsh0225@gmail.com>
2022-12-27 15:04:39 +0900

    study: puppeteer 활용 브라우저 불러오기

--- 학습내용/9.node.js-crawler/lecture/index.js
+++ 학습내용/9.node.js-crawler/lecture/index.js
@@ -1,3 +1,5 @@
+// csv parse
+/*
 const { parse } = require('csv-parse/sync');
 const fs = require('fs');
 
@@ -6,3 +8,32 @@ const records = parse(csv.toString('utf-8'));
 records.forEach((r, i) => {
     console.log(i, r); // r[0] 제목, r[1] 링크
 });
+*/
+
+// puppeteer
+
+const { parse } = require('csv-parse/sync');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const csv = fs.readFileSync('csv/data.csv');
+const records = parse(csv.toString('utf-8'));
+
+const crawler = async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page1 = await browser.newPage();
+    const page2 = await browser.newPage();
+    const page3 = await browser.newPage();
+    await page1.goto('https://naver.com');
+    await page2.goto('https://google.com');
+    await page3.goto('https://daum.net');
+    await page1.waitForTimeout(1000);
+    await page1.close();
+    await page2.waitForTimeout(2000);
+    await page2.close();
+    await page3.waitForTimeout(3000);
+    await page3.close();
+    await browser.close();
+}
+
+crawler();
